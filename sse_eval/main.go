b5b37f780468cb1a99c7d5eaf7e2e8ae301b2339
Tomohiro Takahashi <ttcodegear@gmail.com>
2021-04-25 18:32:07 +0900

    Add files via upload

--- sse_eval/main.go
+++ sse_eval/main.go
@@ -3,7 +3,6 @@ package main
 import (
   "log"
   "net"
-  "math"
   "io"
   "context"
   "github.com/golang/protobuf/proto"
@@ -14,10 +13,107 @@ import (
   pb "sse/pb/sse"
   . "github.com/ahmetb/go-linq/v3"
   mapset "github.com/deckarep/golang-set"
+
+  "math"
+  "errors"
+  "fmt"
+  "os"
+  "io/ioutil"
+  "strings"
+  "os/exec"
+  "bytes"
+  "runtime"
+  "encoding/gob"
+  "reflect"
+  "strconv"
 )
 
 type ConnectorServer struct {
   pb.UnimplementedConnectorServer
+}
+
+type SSEArgs struct {
+  AllArgs [][]interface{}
+}
+
+type SSERetVals struct {
+  RetVals []interface{}
+}
+
+type ParamTuple struct {
+  dtype pb.DataType
+  dual  *pb.Dual
+}
+
+func ExecGoRun(script_txt string, template_name string, all_args *SSEArgs, all_results *SSERetVals) error {
+  tmp_dir := "tmp"
+  if _, err := os.Stat(tmp_dir); os.IsNotExist(err) {
+    os.Mkdir(tmp_dir, 0777)
+  }
+  
+  gobfile, err := ioutil.TempFile(tmp_dir, "temp.*.gob")
+  if err != nil {
+    log.Printf("%v", err)
+    return errors.New("Go Run Error: Failed to create .gob file")
+  }
+  encoder := gob.NewEncoder(gobfile)
+  if err := encoder.Encode(all_args); err != nil {
+    log.Printf("%v", err)
+    return errors.New("Go Run Error: Failed to encode arguments(.gob file)")
+  }
+  gobfile.Close()
+  
+  tempcontent, err := ioutil.ReadFile(template_name)
+  if err != nil {
+    log.Printf("%v", err)
+    return errors.New("Go Run Error: Failed to open template .go file")
+  }
+  gofile, err := ioutil.TempFile(tmp_dir, "temp.*.go")
+  if err != nil {
+    log.Printf("%v", err)
+    return errors.New("Go Run Error: Failed to create .go file")
+  }
+  content := strings.Replace(string(tempcontent), "$$$", script_txt, 1)
+  ioutil.WriteFile(gofile.Name(), []byte(content), os.ModePerm)
+  gofile.Close()
+  
+  var stdout bytes.Buffer
+  var stderr bytes.Buffer
+  var cmd *exec.Cmd
+  goRun := fmt.Sprintf("go run %s %s", gofile.Name(), gobfile.Name())
+  if runtime.GOOS == "windows" {
+    cmd = exec.Command("cmd", "/C", goRun)
+  } else {
+    cmd = exec.Command("bash", "-c", goRun)
+  }
+  cmd.Stdout = &stdout
+  cmd.Stderr = &stderr
+  if err := cmd.Run(); err != nil {
+    log.Printf("%v", err)
+  }
+  result := stdout.String()
+  fmt.Print(result)
+  
+  retfile, err := os.Open(gobfile.Name())
+  if err != nil {
+    log.Printf("%v", err)
+    return errors.New("Go Run Error: Failed to open .gob file for results")
+  }
+  decoder := gob.NewDecoder(retfile)
+  if err := decoder.Decode(all_results); err != nil {
+    log.Printf("%v", err)
+    return errors.New("Go Run Error: Failed to decode results(.gob file)")
+  }
+  retfile.Close()
+
+  if err := os.Remove(retfile.Name()); err != nil {
+    log.Printf("%v", err)
+  }
+  if err := os.Remove(gofile.Name()); err != nil {
+    log.Printf("%v", err)
+  }
+  
+  return nil
 }
 
 // Function Name | Function Type  | Argument     | TypeReturn Type
@@ -27,21 +123,98 @@ func (s *ConnectorServer) ScriptEval(header pb.ScriptRequestHeader, stream pb.Co
   log.Printf("script=" + header.Script)
   // パラメータがあるか否かをチェック
   if( len(header.Params) > 0 ) {
-    return nil
+    for {
+      in, err := stream.Recv()
+      if err == io.EOF {
+        return nil
+      }
+      if err != nil {
+        return err
+      }
+      var all_args SSEArgs = SSEArgs{}
+      for _, row := range in.Rows {
+        script_args := []interface{}{}
+        zip := make([]ParamTuple, len(header.Params), len(header.Params))
+        for i, param := range header.Params {
+          zip[i] = ParamTuple{ param.DataType, row.Duals[i] }
+        }
+        for _, elm := range zip {
+          if  elm.dtype == pb.DataType_NUMERIC || elm.dtype == pb.DataType_DUAL {
+            script_args = append(script_args, elm.dual.NumData)
+          } else {
+            script_args = append(script_args, elm.dual.StrData)
+          }
+        }
+        log.Printf("args=%v", script_args)
+        all_args.AllArgs = append(all_args.AllArgs, script_args)
+      }
+      var all_results *SSERetVals = &SSERetVals{}
+      if err := ExecGoRun(header.Script, "ScriptEval.txt", &all_args, all_results); err != nil {
+        return err
+      }
+      var response_rows pb.BundledRows
+      for _, d := range all_results.RetVals {
+        var result float64 = math.NaN()
+        kind := reflect.TypeOf(d).Kind()
+        val := reflect.ValueOf(d)
+        if kind == reflect.Float64 {
+          result = val.Float()
+        } else if kind == reflect.String {
+          f, err := strconv.ParseFloat(val.String(), 64)
+          if err != nil {
+            f = math.NaN()
+          }
+          result = f
+        } else if kind == reflect.Int || kind == reflect.Int8 || kind == reflect.Int16 || kind == reflect.Int32 || kind == reflect.Int64 {
+          result = float64(val.Int())
+        } else if kind == reflect.Uint || kind == reflect.Uint8 || kind == reflect.Uint16 || kind == reflect.Uint32 || kind == reflect.Uint64 {
+          result = float64(val.Uint())
+        } else {
+          result = math.NaN()
+        }
+        dual := pb.Dual{ NumData: result }
+        r := pb.Row{ Duals: []*pb.Dual{ &dual } }
+        response_rows.Rows = append(response_rows.Rows, &r)
+      }
+      if err := stream.Send(&response_rows); err != nil {
+        return err
+      }
+    }
   } else {
-    var result float64 = math.NaN()
     for {
       _, err := stream.Recv()
       if err == io.EOF {
-        
-        result = 12.34
-        
-        dual := pb.Dual{ NumData: result }
-        row := pb.Row{ Duals: []*pb.Dual{ &dual } }
+        var all_args SSEArgs = SSEArgs{}
+        var all_results *SSERetVals = &SSERetVals{}
+        if err := ExecGoRun(header.Script, "ScriptEval.txt", &all_args, all_results); err != nil {
+          return err
+        }
+        var result float64 = math.NaN()
+        for _, d := range all_results.RetVals {
+          kind := reflect.TypeOf(d).Kind()
+          val := reflect.ValueOf(d)
+          if kind == reflect.Float64 {
+            result = val.Float()
+          } else if kind == reflect.String {
+            f, err := strconv.ParseFloat(val.String(), 64)
+            if err != nil {
+              f = math.NaN()
+            }
+            result = f
+          } else if kind == reflect.Int || kind == reflect.Int8 || kind == reflect.Int16 || kind == reflect.Int32 || kind == reflect.Int64 {
+            result = float64(val.Int())
+          } else if kind == reflect.Uint || kind == reflect.Uint8 || kind == reflect.Uint16 || kind == reflect.Uint32 || kind == reflect.Uint64 {
+            result = float64(val.Uint())
+          } else {
+            result = math.NaN()
+          }
+        }
         var reply pb.BundledRows
-        reply.Rows = append(reply.Rows, &row)
+        dual := pb.Dual{ NumData: result }
+        r := pb.Row{ Duals: []*pb.Dual{ &dual } }
+        reply.Rows = append(reply.Rows, &r)
         if err := stream.Send(&reply); err != nil {
-           return err
+          return err
         }
         return nil
       }
